0dfa8f9c9ea147bfbcbdae2c0851d011609dac77
Gaurav Sahu <contact.gauravsahu@gmail.com>
2023-07-17 12:18:50 +0530

    Leetcode #27 - Optimised

--- src/com/practice/leetcode/RemoveElement.java
+++ src/com/practice/leetcode/RemoveElement.java
@@ -11,10 +11,11 @@ public class RemoveElement {
         //0,0,1,2,2,2,3,4
         int val = 2;
         RemoveElement re = new RemoveElement();
-        System.out.println(re.removeElement(nums, val));
+        System.out.println(re.removeElement2(nums, val));
     }
 
-    public int removeElement(int[] nums, int val) {
+    //With sorting
+    public int removeElement1(int[] nums, int val) {
         Arrays.sort(nums);
         int l = nums.length;
         int j = l - 1;
@@ -29,5 +30,35 @@ public class RemoveElement {
         }
 
         return l - c;
+    }
+
+    //Without sorting
+    public int removeElement2(int[] nums, int val) {
+        int l = nums.length;
+        int i = 0;
+        int j = l - 1;
+
+        while (i < j){
+            if (nums[i] == val) {
+                while (j > i && nums[j] == val) {
+                    j--;
+                }
+
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j--;
+            }
+
+            i++;
+        }
+
+        int k = 0;
+
+        while (k < l && nums[k] != val) {
+            k++;
+        }
+
+        return k;
     }
 }
