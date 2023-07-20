8883d46655fb1e8f1afa10b97e0aab713931a270
Gaurav Sahu <contact.gauravsahu@gmail.com>
2023-07-20 19:17:05 +0530

    Leetcode #2357

--- src/com/practice/leetcode/MakeArrayZero.java
+++ src/com/practice/leetcode/MakeArrayZero.java
@@ -1,6 +1,8 @@
 package com.practice.leetcode;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /*
 2357. Make Array Zero by Subtracting Equal Amounts
@@ -9,10 +11,11 @@ public class MakeArrayZero {
     public static void main(String[] args) {
         MakeArrayZero maz = new MakeArrayZero();
         int[] nums = {1,5,0,3,5};
-        System.out.println(maz.minimumOperations(nums));
+        System.out.println(maz.minimumOperations2(nums));
     }
 
-    public int minimumOperations(int[] nums) {
+    //Using sorting
+    public int minimumOperations1(int[] nums) {
         Arrays.sort(nums);
         int l = nums.length;
         int c = 0;
@@ -33,5 +36,19 @@ public class MakeArrayZero {
         }
 
         return c;
+    }
+
+    //Using HashMap
+    public int minimumOperations2(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num: nums) {
+            if (num != 0) {
+                int freq = map.getOrDefault(num, 0) + 1;
+                map.put(num, freq);
+            }
+        }
+
+        return map.keySet().size();
     }
 }
