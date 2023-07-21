44b8d0cee507151ecb0d66c05ec21333fabba20e
Gaurav Sahu <contact.gauravsahu@gmail.com>
2023-07-21 23:07:51 +0530

    Leetcode #451

--- src/com/practice/leetcode/SortCharsByFreq.java
+++ src/com/practice/leetcode/SortCharsByFreq.java
@@ -1,7 +1,11 @@
 package com.practice.leetcode;
 
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 /*
 451. Sort Characters By Frequency
@@ -10,11 +14,12 @@ public class SortCharsByFreq {
 
     public static void main(String[] args) {
         SortCharsByFreq scbf = new SortCharsByFreq();
-        String s = "tree";
-        System.out.println(scbf.frequencySort(s));
+        String s = "acacc";
+        System.out.println(scbf.frequencySort2(s));
     }
 
-    public String frequencySort(String s) {
+    //Using HashMap
+    public String frequencySort1(String s) {
         Map<Character, Integer> map = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
@@ -33,6 +38,40 @@ public class SortCharsByFreq {
                    sb.append(entry.getKey());
                }
            });
+
+        return sb.toString();
+    }
+
+    //Using PriorityQueue
+    public String frequencySort2(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        Set<Character> set = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            int freq = map.getOrDefault(ch, 0) + 1;
+            map.put(ch, freq);
+            set.add(ch);
+        }
+
+        Comparator<Character> comparator = (ch1, ch2) -> map.get(ch2).compareTo(map.get(ch1));
+        PriorityQueue<Character> pq = new PriorityQueue<>(comparator);
+        pq.addAll(set);
+        StringBuilder sb = new StringBuilder();
+
+        while (!pq.isEmpty()) {
+            char ch = pq.poll();
+
+            if (set.contains(ch)) {
+                int freq = map.get(ch);
+
+                for (int i = 1; i <= freq; i++) {
+                    sb.append(ch);
+                }
+
+                set.remove(ch);
+            }
+        }
 
         return sb.toString();
     }
