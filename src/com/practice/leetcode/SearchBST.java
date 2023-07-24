0ea9027012f9c31d55c5913ffc3339bb4016667e
Gaurav Sahu <contact.gauravsahu@gmail.com>
2023-07-24 19:17:37 +0530

    Leetcode #700

--- src/com/practice/leetcode/SearchBST.java
+++ src/com/practice/leetcode/SearchBST.java
@@ -5,12 +5,28 @@ package com.practice.leetcode;
  */
 public class SearchBST {
 
-    public TreeNode searchBST(TreeNode root, int val) {
+    //Iterative
+    public TreeNode searchBST1(TreeNode root, int val) {
         while (root != null) {
             if (val < root.val) {
                 root = root.left;
             } else if (val > root.val) {
                 root = root.right;
+            } else {
+                return root;
+            }
+        }
+
+        return root;
+    }
+
+    //Recursive
+    public TreeNode searchBST2(TreeNode root, int val) {
+        if (root != null) {
+            if (val < root.val) {
+                root = searchBST2(root.left, val);
+            } else if (val > root.val) {
+                root = searchBST2(root.right, val);
             } else {
                 return root;
             }
