f4005992c3915174a5888ebc2bee3e6e3489d174
Gaurav Sahu <contact.gauravsahu@gmail.com>
2023-07-30 14:03:52 +0530

    Leetcode #88

--- src/com/practice/leetcode/MergeSortedArray.java
+++ src/com/practice/leetcode/MergeSortedArray.java
@@ -8,16 +8,45 @@ import java.util.Arrays;
 public class MergeSortedArray {
     public static void main(String[] args) {
         MergeSortedArray msa = new MergeSortedArray();
-        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums1 = {1,2,3,5,0,0,0};
         int[] nums2 = {2,5,6};
-        int m = 3;
+        int m = 4;
         int n = 3;
-        msa.merge(nums1, m, nums2, n);
+        msa.merge2(nums1, m, nums2, n);
         System.out.println(Arrays.toString(nums1));
     }
 
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    //Using Sorting
+    public void merge1(int[] nums1, int m, int[] nums2, int n) {
         System.arraycopy(nums2, 0, nums1, m, n);
         Arrays.sort(nums1);
+    }
+
+    //Using array
+    public void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0;
+        int j = 0;
+        int c = 0;
+        int[] nums = new int[m+n];
+
+        while (c < m + n) {
+            if (i >= m) {
+                nums[c] = nums2[j];
+                j++;
+            } else if (j >= n) {
+                nums[c] = nums1[i];
+                i++;
+            } else if (nums1[i] <= nums2[j]) {
+                nums[c] = nums1[i];
+                i++;
+            } else {
+                nums[c] = nums2[j];
+                j++;
+            }
+
+            c++;
+        }
+
+        System.arraycopy(nums, 0, nums1, 0, m+n);
     }
 }
