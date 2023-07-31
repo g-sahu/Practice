2c96d21bc7e8940f642f2e18a101ec3a65b9b4ce
Gaurav Sahu <contact.gauravsahu@gmail.com>
2023-07-31 13:42:35 +0530

    Leetcode #217

---
+++ src/com/practice/leetcode/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+package com.practice.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+217. Contains Duplicate
+ */
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int n: nums) {
+            if (!set.add(n)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
