af86d078fc4f6b4dfe195cdb2e8226a9f222d5e9
Gaurav Sahu <contact.gauravsahu@gmail.com>
2023-07-31 21:01:58 +0530

    Leetcode #347

--- src/com/practice/leetcode/TopKFrequent.java
+++ src/com/practice/leetcode/TopKFrequent.java
@@ -1,8 +1,10 @@
 package com.practice.leetcode;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 /*
 347. Top K Frequent Elements
@@ -10,11 +12,12 @@ import java.util.Map;
 public class TopKFrequent {
     public static void main(String[] args) {
         TopKFrequent tf = new TopKFrequent();
-        int[] nums = {1,1,1,2,2,3};
-        System.out.println(Arrays.toString(tf.topKFrequent(nums, 2)));
+        int[] nums = {4,1,-1,2,-1,2,3};
+        System.out.println(Arrays.toString(tf.topKFrequent2(nums, 2)));
     }
 
-    public int[] topKFrequent(int[] nums, int k) {
+    //Using HashMap and sorting
+    public int[] topKFrequent1(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int n : nums) {
@@ -29,5 +32,33 @@ public class TopKFrequent {
                   .map(Map.Entry :: getKey)
                   .mapToInt(Integer :: intValue)
                   .toArray();
+    }
+
+    //Using Priority Queue
+    public int[] topKFrequent2(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int n : nums) {
+            int val = map.getOrDefault(n, 0) + 1;
+            map.put(n, val);
+        }
+
+        Comparator<Integer> comparator = Comparator.comparing(map :: get);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(comparator);
+
+        map.forEach((key, val) -> {
+            if (pq.size() == k) {
+                if (map.get(key).compareTo(map.get(pq.peek())) > 0) {
+                    pq.poll();
+                    pq.offer(key);
+                }
+            } else {
+                pq.offer(key);
+            }
+        });
+
+        return pq.stream()
+                 .mapToInt(Integer :: intValue)
+                 .toArray();
     }
 }
