7cc6b564c19da43671899dddf61e619e02d57570
Gaurav Sahu <contact.gauravsahu@gmail.com>
2023-07-31 19:38:15 +0530

    Leetcode #347

--- src/com/practice/leetcode/TopKFrequent.java
+++ src/com/practice/leetcode/TopKFrequent.java
@@ -2,9 +2,7 @@ package com.practice.leetcode;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /*
 347. Top K Frequent Elements
@@ -19,23 +17,17 @@ public class TopKFrequent {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
 
-        for (int n: nums) {
+        for (int n : nums) {
             int val = map.getOrDefault(n, 0) + 1;
             map.put(n, val);
         }
 
-        int[] topK = new int[k];
-
-        List<Integer> list = map.entrySet()
-                                .stream()
-                                .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
-                                .map(Map.Entry :: getKey)
-                                .collect(Collectors.toList());
-
-        for (int i = 0; i < k; i++) {
-            topK[i] = list.get(i);
-        }
-
-        return topK;
+        return map.entrySet()
+                  .stream()
+                  .sorted((e1, e2) -> e2.getValue().compareTo(e1.getValue()))
+                  .limit(k)
+                  .map(Map.Entry :: getKey)
+                  .mapToInt(Integer :: intValue)
+                  .toArray();
     }
 }
