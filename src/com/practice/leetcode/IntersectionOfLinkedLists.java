14acf4f1f436b58e4304ccbd84251532df6c3942
Gaurav Sahu <contact.gauravsahu@gmail.com>
2023-08-01 11:37:56 +0530

    Leetcode #160

--- src/com/practice/leetcode/IntersectionOfLinkedLists.java
+++ src/com/practice/leetcode/IntersectionOfLinkedLists.java
@@ -13,11 +13,12 @@ public class IntersectionOfLinkedLists {
         int[] list2 = {5,6,1,8,4,5};
         ListNode head1 = LeetUtils.buildLinkedList(list1);
         ListNode head2 = LeetUtils.buildLinkedList(list2);
-        ListNode intersectionNode = intersection.getIntersectionNode(head1, head2);
+        ListNode intersectionNode = intersection.getIntersectionNode2(head1, head2);
         LeetUtils.printLinkedList(intersectionNode);
     }
 
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    //Using HashSet
+    public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
         Set<ListNode> set = new HashSet<>();
 
         while (headA != null) {
@@ -33,6 +34,43 @@ public class IntersectionOfLinkedLists {
         }
 
         return null;
+    }
+
+    //Using two pointers
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        int m = countNodes(headA);
+        int n = countNodes(headB);
+        int diff = Math.abs(m-n);
+
+        if (m > n) {
+            for (int i = 1; i <= diff; i++) {
+                headA = headA.next;
+            }
+        } else if (m < n) {
+            for (int i = 1; i <= diff; i++) {
+                headB = headB.next;
+            }
+        }
+
+        while (headA != null) {
+            if (headA == headB) {
+                return headA;
+            }
+
+            headA = headA.next;
+            headB = headB.next;
+        }
+
+        return null;
+    }
+
+    private int countNodes(ListNode head) {
+        int n = 0;
+        while (head != null) {
+            n++;
+            head = head.next;
+        }
+        return n;
     }
 
 }
