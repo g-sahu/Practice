06600d91054173a824d1a5ff8b03352658be69c6
Gaurav Sahu <simplygaurav07@gmail.com>
2023-08-13 11:09:23 +0530

    Leetcode #56

--- src/com/practice/leetcode/MergeIntervals.java
+++ src/com/practice/leetcode/MergeIntervals.java
@@ -2,6 +2,7 @@ package com.practice.leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 /*
@@ -12,10 +13,8 @@ public class MergeIntervals {
     public static void main(String[] args) {
         MergeIntervals mi = new MergeIntervals();
         int[][] intervals = {
-                {1,3},
-                {2,6},
-                {8,10},
-                {15,18}
+                {1,4},
+                {1,4}
         };
 
         int[][] merge = mi.merge(intervals);
@@ -23,39 +22,55 @@ public class MergeIntervals {
     }
 
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        int l = intervals.length;
+
+        if (l < 2) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
 
         for (int[] row: intervals) {
             Arrays.sort(row);
         }
 
-        List<int[]> list = new ArrayList<>();
-        int i = 0;
+        List<int[]> merged = new ArrayList<>();
+        int i = 1;
 
-        while (i < intervals.length - 1) {
+        while (i < l) {
+            int[] prevRow = intervals[i-1];
             int[] currentRow = intervals[i];
-            int[] nextRow = intervals[i+1];
-            int start = currentRow[0];
-            int end = currentRow[1];
-            int j = 0;
+            int start = prevRow[0];
+            int end = prevRow[1];
 
-            while (currentRow[1] > nextRow[0]) {
-                end = nextRow[1];
-                j++;
-                currentRow = intervals[j];
-                nextRow = intervals[j+1];
+            while (currentRow[0] <= end) {
+                if (currentRow[1] > end) {
+                    end = currentRow[1];
+                }
+
+                i++;
+
+                if (i == l) {
+                    break;
+                }
+
+                currentRow = intervals[i];
             }
 
-            list.add(new int[]{start, end});
-            i = j+1;
+            merged.add(new int[]{start, end});
+
+            if (i == l-1) {
+                merged.add(intervals[i]);
+            }
+
+            i++;
         }
 
-        int[][] merge = new int[list.size()][2];
-
-        for (int k = 0; k < list.size(); k++) {
-            merge[k] = list.get(k);
+        int[][] mergedIntervals = new int[merged.size()][2];
+        for (int j = 0; j < merged.size(); j++) {
+            mergedIntervals[j] = merged.get(j);
         }
 
-        return merge;
+        return mergedIntervals;
     }
 }
