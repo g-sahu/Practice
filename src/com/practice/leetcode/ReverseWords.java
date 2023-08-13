4456a7ef638465d0f0fed4198578e2115a1fdcea
Gaurav Sahu <simplygaurav07@gmail.com>
2023-08-13 18:00:52 +0530

    Leetcode #151

---
+++ src/com/practice/leetcode/ReverseWords.java
@@ -0,0 +1,23 @@
+package com.practice.leetcode;
+
+/*
+151. Reverse Words in a String
+ */
+public class ReverseWords {
+
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = words.length-1; i >= 0; i--) {
+            String str = words[i];
+
+            if (!str.isEmpty()) {
+                sb.append(" ").append(str);
+            }
+        }
+
+        sb.deleteCharAt(0);
+        return sb.toString();
+    }
+}
