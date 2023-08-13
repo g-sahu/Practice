fcb416638ef9e10cf533c8ade0e2fb72e0382b24
Gaurav Sahu <simplygaurav07@gmail.com>
2023-08-13 17:49:21 +0530

    Leetcode #1768

--- src/com/practice/leetcode/MergeStringsAlternately.java
+++ src/com/practice/leetcode/MergeStringsAlternately.java
@@ -5,9 +5,10 @@ package com.practice.leetcode;
  */
 public class MergeStringsAlternately {
 
-    public String mergeAlternately(String word1, String word2) {
-        int l = word1.length() + word2.length();
-        StringBuilder sb = new StringBuilder(l);
+    //Using StringBuilder
+    public String mergeAlternately1(String word1, String word2) {
+        int l = Math.max(word1.length(), word2.length());
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < l; i++) {
             if (i < word1.length()) {
@@ -20,5 +21,24 @@ public class MergeStringsAlternately {
         }
 
         return sb.toString();
+    }
+
+    //Using Array
+    public String mergeAlternately2(String word1, String word2) {
+        int l = Math.max(word1.length(), word2.length());
+        char[] arr = new char[word1.length() + word2.length()];
+        int j = 0;
+
+        for (int i = 0; i < l; i++) {
+            if (i < word1.length()) {
+                arr[j++] = word1.charAt(i);
+            }
+
+            if (i < word2.length()) {
+                arr[j++] = word2.charAt(i);
+            }
+        }
+
+        return new String(arr);
     }
 }
