f6677f6cd3e4660ac1c7416fccea0d69f20791e2
Gaurav Sahu <simplygaurav07@gmail.com>
2023-08-14 21:06:55 +0530

    Selection Sort

--- src/com/practice/sorting/SelectionSort.java
+++ src/com/practice/sorting/SelectionSort.java
@@ -1,17 +1,30 @@
 package com.practice.sorting;
 
+import java.util.Arrays;
+
 public class SelectionSort {
 
 	public static void main(String[] args) {
-		int arr[] = {5, 7, 3, 8, 6, 10, 23, 14, 18};
-		int l = arr.length;
-		int minimum;
-		
-		for(int i=0; i<l; i++) {
-			minimum = arr[i];
-			
-		}
-
+		int[] arr = {15,7,3,8,6,1,23,14,18};
+		selectionSort(arr);
+		System.out.println(Arrays.toString(arr));
 	}
 
+	private static void selectionSort(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			int minIndex = i;
+
+			for (int j = i+1; j < arr.length; j++) {
+				if (arr[j] < arr[minIndex]) {
+					minIndex = j;
+				}
+			}
+
+			if (i != minIndex) {
+				int temp = arr[i];
+				arr[i] = arr[minIndex];
+				arr[minIndex] = temp;
+			}
+		}
+	}
 }
