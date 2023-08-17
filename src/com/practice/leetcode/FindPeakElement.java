cb188494d3b2d2766f2c5c9669978d81ec2c2404
Gaurav Sahu <simplygaurav07@gmail.com>
2023-08-17 20:11:48 +0530

    Leetcode #162

--- src/com/practice/leetcode/FindPeakElement.java
+++ src/com/practice/leetcode/FindPeakElement.java
@@ -11,7 +11,7 @@ public class FindPeakElement {
     }
 
     public int findPeakElement(int[] nums) {
-        int last = nums.length-1;
+        int last = nums.length - 1;
 
         if (last == 0) {
             return 0;
@@ -21,28 +21,22 @@ public class FindPeakElement {
         int right = last;
 
         while (left <= right) {
-            int mid = (left + right)/2;
+            int mid = (left + right) / 2;
 
             if (mid == 0) {
-                if (nums[mid] > nums[mid+1]) {
+                if (nums[mid] > nums[mid + 1]) {
                     return mid;
                 } else {
-                    left = mid+1;
+                    left = mid + 1;
                 }
-            } else if (mid == last) {
-                if (nums[mid] > nums[mid-1]) {
+            } else if (mid == last || nums[mid] > nums[mid + 1]) {
+                if (nums[mid] > nums[mid - 1]) {
                     return mid;
                 } else {
-                    right = mid-1;
-                }
-            } else if (nums[mid] > nums[mid+1]) {
-                if (nums[mid] > nums[mid-1]) {
-                    return mid;
-                } else {
-                    right = mid-1;
+                    right = mid - 1;
                 }
             } else {
-                left = mid+1;
+                left = mid + 1;
             }
         }
 
