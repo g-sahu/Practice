74490921b0c718049211481015437f0dd39c8f6e
Gaurav Sahu <simplygaurav07@gmail.com>
2023-08-25 23:07:20 +0530

    Leetcode #1323

--- src/com/practice/leetcode/Maximum69Number.java
+++ src/com/practice/leetcode/Maximum69Number.java
@@ -17,15 +17,14 @@ public class Maximum69Number {
             num /= 10;
         }
 
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) == 6) {
-                list.set(i, 9);
-                break;
-            }
-        }
-
         int n = 0;
+        boolean flag = false;
+
         for (int x : list) {
+            if (!flag && x == 6) {
+                x = 9;
+                flag = true;
+            }
             n = n * 10 + x;
         }
 
@@ -39,22 +38,20 @@ public class Maximum69Number {
         return Integer.parseInt(n);
     }
 
-    //Using char array 9669
+    //Using char array
     public int maximum69Number3(int num) {
         char[] digits = Integer.toString(num).toCharArray();
-
-        for (int i = 0; i < digits.length; i++) {
-            if (digits[i] == '6') {
-                digits[i] = '9';
-                break;
-            }
-        }
-
         int n = 0;
-        for (char x : digits) {
-            n = (n * 10) + (x - '0');
-        }
+        boolean flag = false;
 
+        for (char digit : digits) {
+            if (!flag && digit == '6') {
+                digit = '9';
+                flag = true;
+            }
+
+            n = (n * 10) + (digit - '0');
+        }
         return n;
     }
 }
