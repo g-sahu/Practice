396081b137f4c7842c45e0aebe3a96c2e1f7ae8d
Gaurav Sahu <simplygaurav07@gmail.com>
2023-08-31 14:10:45 +0530

    Leetcode #2352

--- src/com/practice/leetcode/EqualRowColumnPairs.java
+++ src/com/practice/leetcode/EqualRowColumnPairs.java
@@ -1,12 +1,16 @@
 package com.practice.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 2352. Equal Row and Column Pairs
  */
 public class EqualRowColumnPairs {
 
     //Brute force
-    public int equalPairs(int[][] grid) {
+    public int equalPairs1(int[][] grid) {
         int n = grid.length;
         int c = 0;
 
@@ -30,5 +34,32 @@ public class EqualRowColumnPairs {
         }
 
         return c;
+    }
+
+    //Using HashMap
+    public int equalPairs2(int[][] grid) {
+        int l = grid.length;
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int[] row : grid) {
+            String key = Arrays.toString(row);
+            int freq = map.getOrDefault(key, 0) + 1;
+            map.put(key, freq);
+        }
+
+        int sum = 0;
+
+        for (int j = 0; j < l; j++) {
+            int[] col = new int[l];
+
+            for (int i = 0; i < l; i++) {
+                col[i] = grid[i][j];
+            }
+
+            String key = Arrays.toString(col);
+            sum += map.getOrDefault(key, 0);
+        }
+
+        return sum;
     }
 }
