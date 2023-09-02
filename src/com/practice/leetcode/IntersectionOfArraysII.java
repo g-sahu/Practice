5183f655b2091feaa90311c753c9161f41ac4104
Gaurav Sahu <simplygaurav07@gmail.com>
2023-09-02 17:26:49 +0530

    Leetcode #350

--- src/com/practice/leetcode/IntersectionOfArraysII.java
+++ src/com/practice/leetcode/IntersectionOfArraysII.java
@@ -1,6 +1,7 @@
 package com.practice.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,7 +12,7 @@ import java.util.Map;
 public class IntersectionOfArraysII {
 
     //Using HashMap
-    public int[] intersect(int[] nums1, int[] nums2) {
+    public int[] intersect1(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
         List<Integer> list = new ArrayList<>();
 
@@ -32,5 +33,27 @@ public class IntersectionOfArraysII {
         return list.stream()
                    .mapToInt(Integer :: intValue)
                    .toArray();
+    }
+
+    //Using two pointers
+    public int[] intersect2(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i=0, j=0;
+        List<Integer> list = new ArrayList<>();
+
+        while(i < nums1.length && j < nums2.length) {
+            if(nums1[i] < nums2[j]) {
+                i++;
+            } else if(nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
