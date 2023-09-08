7f821942dc1f2c9a4cb0bd8a1474a34f6dbcb68d
Gaurav Sahu <simplygaurav07@gmail.com>
2023-09-08 13:02:47 +0530

    Oracle interview

---
+++ src/com/interviews/oracle/FindTheNumber.java
@@ -0,0 +1,16 @@
+package com.interviews.oracle;
+
+/*
+Given a sorted array where all numbers are repeated twice except one which is repeated thrice, find the number repeated thrice.
+Time Complexity - O(log n)
+
+Example -
+Input - [2,2,3,3,4,4,5,5,8,8,8,9,9]
+Ans - 8
+ */
+public class FindTheNumber {
+
+    public int findTheNumber(int[] nums) {
+        return 0;
+    }
+}
