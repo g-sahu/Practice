a36631810e2287e0e67c2e6f3914a9f9645d39c8
Gaurav Sahu <simplygaurav07@gmail.com>
2023-09-14 19:23:22 +0530

    Leetcode #509

--- src/com/practice/leetcode/FibonacciNumber.java
+++ src/com/practice/leetcode/FibonacciNumber.java
@@ -5,40 +5,15 @@ import java.util.Map;
 
 /*
 509. Fibonacci Number
-The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
-such that each number is the sum of the two preceding ones, starting from 0 and 1.
-
-That is,
-F(0) = 0, F(1) = 1
-F(n) = F(n - 1) + F(n - 2), for n > 1.
-
-Given n, calculate F(n).
-
-Example 1:
-Input: n = 2
-Output: 1
-Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
-
-Example 2:
-Input: n = 3
-Output: 2
-Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
-
-Example 3:
-Input: n = 4
-Output: 3
-Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
-
-Constraints:
-0 <= n <= 30
  */
 public class FibonacciNumber {
     public static void main(String[] args) {
         FibonacciNumber fn = new FibonacciNumber();
-        System.out.println(fn.fib(4));
+        System.out.println(fn.fib2(6));
     }
 
-    public int fib(int n) {
+    //DP: Top-down
+    public int fib1(int n) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, 0);
         map.put(1, 1);
@@ -51,6 +26,20 @@ public class FibonacciNumber {
         }
 
         map.put(n, fib(n-1, map) + fib(n-2, map));
+        return map.get(n);
+    }
+
+    //DP: Bottom-up
+    public int fib2(int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 0);
+        map.put(1, 1);
+
+        for (int i = 2; i <= n; i++) {
+            int fib = map.get(i-1) + map.get(i-2);
+            map.put(i, fib);
+        }
+
         return map.get(n);
     }
 }
