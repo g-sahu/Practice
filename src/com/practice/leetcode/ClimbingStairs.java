4868aec69e9be8ec411469c53b9cdd657e5d5d3e
Gaurav Sahu <simplygaurav07@gmail.com>
2023-09-15 18:59:44 +0530

    Leetcode #70

--- src/com/practice/leetcode/ClimbingStairs.java
+++ src/com/practice/leetcode/ClimbingStairs.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class ClimbingStairs {
 
     //DP: Top-Down
-    public int climbStairs(int n) {
+    public int climbStairs1(int n) {
         Map<Integer, Integer> map = new HashMap<>();
         map.put(1, 1);
         map.put(2, 2);
@@ -24,5 +24,22 @@ public class ClimbingStairs {
         int ways = dp(n-1, map) + dp(n-2, map);
         map.put(n, ways);
         return ways;
+    }
+
+    //DP: Bottom-Up
+    public int climbStairs2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 }
