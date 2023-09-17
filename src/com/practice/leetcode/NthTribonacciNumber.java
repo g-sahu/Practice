9fecebcd744738f231c3487457700c0711dc52c0
Gaurav Sahu <simplygaurav07@gmail.com>
2023-09-17 12:16:09 +0530

    Leetcode #1137

--- src/com/practice/leetcode/NthTribonacciNumber.java
+++ src/com/practice/leetcode/NthTribonacciNumber.java
@@ -9,7 +9,8 @@ import java.util.Map;
 public class NthTribonacciNumber {
     private final Map<Integer, Integer> map = new HashMap<>();
 
-    public int tribonacci(int n) {
+    //DP: Top-down
+    public int tribonacci1(int n) {
         if(n == 0) {
             return 0;
         }
@@ -20,8 +21,26 @@ public class NthTribonacciNumber {
             return map.get(n);
         }
 
-        int t = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+        int t = tribonacci1(n-1) + tribonacci1(n-2) + tribonacci1(n-3);
         map.put(n, t);
         return t;
+    }
+
+    //DP: Bottom-up
+    public int tribonacci2(int n) {
+        if (n < 3) {
+            return n == 0 ? 0 : 1;
+        }
+
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+
+        return dp[n];
     }
 }
