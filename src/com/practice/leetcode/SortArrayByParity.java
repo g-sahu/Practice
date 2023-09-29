73eefa2f1561704aba71afeabed415d19d45fdd7
Gaurav Sahu <simplygaurav07@gmail.com>
2023-09-29 20:31:56 +0530

    Leetcode #905

--- src/com/practice/leetcode/SortArrayByParity.java
+++ src/com/practice/leetcode/SortArrayByParity.java
@@ -6,7 +6,7 @@ package com.practice.leetcode;
 public class SortArrayByParity {
 
     //Brute force
-    public int[] sortArrayByParity(int[] nums) {
+    public int[] sortArrayByParity1(int[] nums) {
         for(int i=0; i<nums.length; i++) {
             if(nums[i] % 2 != 0) {
                 for(int j=i+1; j<nums.length; j++) {
@@ -16,6 +16,28 @@ public class SortArrayByParity {
                         nums[j] = t;
                     }
                 }
+            }
+        }
+
+        return nums;
+    }
+
+    //Two pointers
+    public int[] sortArrayByParity2(int[] nums) {
+        int l=0, r= nums.length-1;
+
+        while (l < r) {
+            if (nums[l] % 2 != 0) {
+                if (nums[r] %2 == 0) {
+                    int t = nums[l];
+                    nums[l] = nums[r];
+                    nums[r] = t;
+                    l++;
+                } else {
+                    r--;
+                }
+            } else {
+                l++;
             }
         }
 
