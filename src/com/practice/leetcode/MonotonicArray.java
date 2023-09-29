24b75cbe4a2d04f8877f11c3c8a96bcf28b5e697
Gaurav Sahu <simplygaurav07@gmail.com>
2023-09-29 20:31:46 +0530

    Leetcode #896

--- src/com/practice/leetcode/MonotonicArray.java
+++ src/com/practice/leetcode/MonotonicArray.java
@@ -1,17 +1,17 @@
 package com.practice.leetcode;
 
-/*
-896. Monotonic Array
+/**
+ * 896. Monotonic Array
  */
 public class MonotonicArray {
 
     public static void main(String[] args) {
         int[] nums = {1,2,2,3};
         MonotonicArray o = new MonotonicArray();
-        System.out.println(o.isMonotonic(nums));
+        System.out.println(o.isMonotonic1(nums));
     }
 
-    public boolean isMonotonic(int[] nums) {
+    public boolean isMonotonic1(int[] nums) {
         int comp = 0;
 
         for (int i = 0; i < nums.length - 1; i++) {
@@ -21,6 +21,27 @@ public class MonotonicArray {
                 comp = c;
             } else {
                 if (c != 0 && c != comp) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isMonotonic2(int[] nums) {
+        int l = nums.length;
+        boolean inc = nums[0] < nums[l-1];
+
+        if(inc) {
+            for(int i=0; i<l-1; i++) {
+                if(nums[i] > nums[i+1]) {
+                    return false;
+                }
+            }
+        } else {
+            for(int i=0; i<l-1; i++) {
+                if(nums[i] < nums[i+1]) {
                     return false;
                 }
             }
