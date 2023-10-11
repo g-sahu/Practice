16765627e665a3f6d84ed24597c70e4ccbe15afe
Gaurav Sahu <simplygaurav07@gmail.com>
2023-10-11 20:28:32 +0530

    Leetcode #94

--- src/com/practice/leetcode/BinaryTreeInorderTraversal.java
+++ src/com/practice/leetcode/BinaryTreeInorderTraversal.java
@@ -3,19 +3,22 @@ package com.practice.leetcode;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-94. Binary Tree Inorder Traversal
+/**
+ * 94. Binary Tree Inorder Traversal
  */
 public class BinaryTreeInorderTraversal {
-    private final List<Integer> list = new ArrayList<>();
+    List<Integer> list = new ArrayList<>();
 
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root != null) {
-            inorderTraversal(root.left);
-            list.add(root.val);
-            inorderTraversal(root.right);
-        }
+        traverse(root);
         return list;
     }
 
+    private void traverse(TreeNode node) {
+        if(node != null) {
+            traverse(node.left);
+            list.add(node.val);
+            traverse(node.right);
+        }
+    }
 }
