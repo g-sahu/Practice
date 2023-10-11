6cc9b1d3832259066402e32850b52440ec200e23
Gaurav Sahu <simplygaurav07@gmail.com>
2023-10-11 18:09:40 +0530

    Leetcode #209

--- src/com/practice/leetcode/MinimumSubArray.java
+++ src/com/practice/leetcode/MinimumSubArray.java
@@ -1,73 +1,29 @@
 package com.practice.leetcode;
 
-/*
-    209. Minimum Size Subarray Sum
-    Given an array of positive integers nums and a positive integer target, return the minimal length of a
-    subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
-
-    Example 1:
-    Input: target = 7, nums = [2,3,1,2,4,3]
-    Output: 2
-    Explanation: The subarray [4,3] has the minimal length under the problem constraint.
-
-    Example 2:
-    Input: target = 4, nums = [1,4,4]
-    Output: 1
-
-    Example 3:
-    Input: target = 11, nums = [1,1,1,1,1,1,1,1]
-    Output: 0
-
-    Constraints:
-    1 <= target <= 109
-    1 <= nums.length <= 105
-    1 <= nums[i] <= 104
-
-    Follow up: If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log(n)).
+/**
+ * 209. Minimum Size Subarray Sum
  */
 public class MinimumSubArray {
     public static void main(String[] args) {
         MinimumSubArray ms = new MinimumSubArray();
-        int target = 11;
-        int[] nums = {1,1,1,1,1,1,1,1};
-        System.out.println(ms.minSubArrayLen(target, nums));
+        int k = 7;
+        int[] nums = {2,3,1,2,4,3};
+        System.out.println(ms.minSubArrayLen(k, nums));
     }
 
-    public int minSubArrayLen(int target, int[] nums) {
-        int min = 0;
-        int sum = 0;
-        int i = 0;
-
-        while (i < nums.length) {
-            sum += nums[i];
-
-            if (sum >= target) {
-                min = i + 1;
-                break;
-            }
-
-            i++;
-        }
-
-        int j = i;
-        i = 1;
+    public int minSubArrayLen(int k, int[] nums) {
+        int i=0, j=0;
+        int sum=0, min=Integer.MAX_VALUE;
 
         while (j < nums.length) {
-            sum -= nums[i - 1];
+            sum += nums[j++];
 
-            if (sum >= target) {
-                --min;
-            } else {
-                j++;
-
-                if (j < nums.length) {
-                    sum += nums[j];
-                }
+            while (sum >= k) {
+                min = Math.min(min, j-i);
+                sum -= nums[i++];
             }
-
-            i++;
         }
 
-        return min;
+        return min == Integer.MAX_VALUE ? 0 : min;
     }
 }
