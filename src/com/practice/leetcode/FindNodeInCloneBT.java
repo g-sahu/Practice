27d77b236da04c919510a9fece0686dbf0d78031
Gaurav Sahu <simplygaurav07@gmail.com>
2023-10-18 19:01:54 +0530

    Leetcode #1379

--- src/com/practice/leetcode/FindNodeInCloneBT.java
+++ src/com/practice/leetcode/FindNodeInCloneBT.java
@@ -1,12 +1,16 @@
 package com.practice.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * 1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree
  */
 public class FindNodeInCloneBT {
     TreeNode ans;
 
-    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+    //Using DFS
+    public final TreeNode getTargetCopy1(final TreeNode original, final TreeNode cloned, final TreeNode target) {
         dfs(cloned, target);
         return ans;
     }
@@ -19,5 +23,31 @@ public class FindNodeInCloneBT {
             dfs(node.left, target);
             dfs(node.right, target);
         }
+    }
+
+    //Using BFS
+    public final TreeNode getTargetCopy2(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(cloned);
+
+        while (!q.isEmpty()) {
+            int n = q.size();
+
+            for (int i = 1; i <= n; i++) {
+                TreeNode node = q.poll();
+
+                if (node.val == target.val) {
+                    return node;
+                }
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+        }
+
+        return null;
     }
 }
