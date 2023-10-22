bd317c26b9534b9dd0587c849efa117e46db30fd
Gaurav Sahu <simplygaurav07@gmail.com>
2023-10-22 22:34:51 +0530

    Leetcode #98

--- src/com/practice/leetcode/ValidateBST.java
+++ src/com/practice/leetcode/ValidateBST.java
@@ -1,34 +1,26 @@
 package com.practice.leetcode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 98. Validate Binary Search Tree
  */
 public class ValidateBST {
-    List<Integer> list = new ArrayList<>();
+    Integer prev;
+    boolean isValid = true;
 
     public boolean isValidBST(TreeNode root) {
         dfs(root);
-        int prev = list.get(0);
-
-        for(int i=1; i<list.size(); i++) {
-            int n = list.get(i);
-
-            if(n <= prev) {
-                return false;
-            }
-            prev = n;
-        }
-
-        return true;
+        return isValid;
     }
 
     void dfs(TreeNode node) {
         if(node != null) {
             dfs(node.left);
-            list.add(node.val);
+
+            if(prev != null && node.val <= prev) {
+                isValid = false;
+            }
+            prev = node.val;
+
             dfs(node.right);
         }
     }
