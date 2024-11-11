e0c5f404af089d480eb499955fc967299e1744d2
Gaurav Sahu <simplygaurav07@gmail.com>
2024-11-11 19:39:38 +0530

    Leetcode #1302

--- src/com/practice/leetcode/DeepestLeavesSum.java
+++ src/com/practice/leetcode/DeepestLeavesSum.java
@@ -1,12 +1,16 @@
 package com.practice.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * 1302. Deepest Leaves Sum
  */
 public class DeepestLeavesSum {
     int max, sum;
 
-    public int deepestLeavesSum(TreeNode root) {
+    //DFS
+    public int deepestLeavesSum1(TreeNode root) {
         preOrder(root, 0);
         return sum;
     }
@@ -22,5 +26,31 @@ public class DeepestLeavesSum {
             preOrder(node.left, l+1);
             preOrder(node.right, l+1);
         }
+    }
+
+    //BFS
+    public int deepestLeavesSum2(TreeNode root) {
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.offer(root);
+        int sum=0;
+
+        while(!q.isEmpty()) {
+            int n = q.size();
+            sum=0;
+
+            for(int i=0; i<n; i++) {
+                TreeNode node = q.poll();
+                sum += node.val;
+
+                if(node.left != null) {
+                    q.offer(node.left);
+                }
+                if(node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+        }
+
+        return sum;
     }
 }
