bce7d62df23bb3869afe0eae9bae2405a6191798
Gaurav Sahu <simplygaurav07@gmail.com>
2024-11-15 19:33:19 +0530

    Leetcode #1845

---
+++ src/com/practice/leetcode/SeatReservationManager.java
@@ -0,0 +1,26 @@
+package com.practice.leetcode;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * 1845. Seat Reservation Manager
+ */
+public class SeatReservationManager {
+    private Queue<Integer> pq;
+
+    public SeatManager(int n) {
+        pq = new PriorityQueue<>();
+        for(int i=1; i<=n; i++) {
+            pq.offer(i);
+        }
+    }
+
+    public int reserve() {
+        return pq.poll();
+    }
+
+    public void unreserve(int seatNumber) {
+        pq.offer(seatNumber);
+    }
+}
