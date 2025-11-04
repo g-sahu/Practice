d2e6ae831b594846d5cef838a3ff8ae0ffd2334e
Gaurav Sahu <simplygaurav07@gmail.com>
2025-11-04 19:57:28 +0530

    Leetcode #230

--- src/com/practice/leetcode/KthSmallestElementInBST.java
+++ src/com/practice/leetcode/KthSmallestElementInBST.java
@@ -8,17 +8,40 @@ import java.util.List;
  */
 public class KthSmallestElementInBST {
     List<Integer> list = new ArrayList<>();
+    int c=0;
 
     public int kthSmallest(TreeNode root, int k) {
-        inorder(root);
+        inorder1(root);
         return list.get(k-1);
     }
 
-    void inorder(TreeNode node) {
+    //With extra space
+    void inorder1(TreeNode node) {
         if(node != null) {
-            inorder(node.left);
+            inorder1(node.left);
             list.add(node.val);
-            inorder(node.right);
+            inorder1(node.right);
         }
+    }
+
+    //Without extra space
+    int inorder2(TreeNode node, int k) {
+        if(node != null) {
+            int r = inorder2(node.left, k);
+
+            if(r == -1) {
+                c++;
+
+                if(c == k) {
+                    return node.val;
+                }
+
+                return inorder2(node.right, k);
+            } else {
+                return r;
+            }
+        }
+
+        return -1;
     }
 }
