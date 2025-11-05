005b9b7298433fb328ba9e00b3e2295d31ab5c2d
Gaurav Sahu <simplygaurav07@gmail.com>
2025-11-05 22:36:02 +0530

    Leetcode #236

--- src/com/practice/leetcode/LCAOfABinaryTree.java
+++ src/com/practice/leetcode/LCAOfABinaryTree.java
@@ -1,56 +1,51 @@
 package com.practice.leetcode;
 
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 236. Lowest Common Ancestor of a Binary Tree
  */
 public class LCAOfABinaryTree {
-    Stack<TreeNode> s1 = new Stack<>();
-    Stack<TreeNode> s2 = new Stack<>();
-
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        preorder(root, p, s1);
-        preorder(root, q, s2);
+        List<TreeNode> l1 = new ArrayList<>();
+        List<TreeNode> l2 = new ArrayList<>();
+        preorder(root, p, l1);
+        preorder(root, q, l2);
 
-        int s = Math.min(s1.size(), s2.size());
+        int s = Math.min(l1.size(), l2.size());
         TreeNode lca = root;
 
         for(int i=0; i<s; i++) {
-            TreeNode n1 = s1.get(i);
-            TreeNode n2 = s2.get(i);
+            TreeNode n1 = l1.get(i);
+            TreeNode n2 = l2.get(i);
 
-            if(n1.val == n2.val) {
-                lca = n1;
-            } else {
-                return lca;
+            if(n1 != n2) {
+                break;
             }
+
+            lca = n1;
         }
 
         return lca;
     }
 
-    boolean preorder(TreeNode node, TreeNode x, Stack<TreeNode> stack) {
-        if(node != null) {
-            stack.push(node);
-
-            if(node.val == x.val) {
-                return true;
-            }
-
-            boolean found = preorder(node.left, x, stack);
-
-            if(!found) {
-                found = preorder(node.right, x, stack);
-
-                if(!found) {
-                    stack.pop();
-                }
-            }
-
-            return found;
+    boolean preorder(TreeNode node, TreeNode x, List<TreeNode> list) {
+        if(node == null) {
+            return false;
         }
 
+        list.add(node);
+
+        if(node == x) {
+            return true;
+        }
+
+        if(preorder(node.left, x, list) || preorder(node.right, x, list)) {
+            return true;
+        }
+
+        list.remove(list.size() - 1);
         return false;
     }
 }
