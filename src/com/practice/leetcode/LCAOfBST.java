db2ac972b8bfddd2c13703048387a61e088dcb83
Gaurav Sahu <simplygaurav07@gmail.com>
2025-11-05 22:56:17 +0530

    Leetcode #235

---
+++ src/com/practice/leetcode/LCAOfBST.java
@@ -0,0 +1,18 @@
+package com.practice.leetcode;
+
+/**
+ * 235. Lowest Common Ancestor of a Binary Search Tree
+ */
+public class LCAOfBST {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if(p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+
+}
