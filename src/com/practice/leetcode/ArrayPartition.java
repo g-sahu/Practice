a77a7d79f0346562e2e215650d053a74c947d4b0
Gaurav Sahu <simplygaurav07@gmail.com>
2025-12-04 17:36:18 +0530

    Leetcode #561

---
+++ src/com/practice/leetcode/ArrayPartition.java
@@ -0,0 +1,19 @@
+package com.practice.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 561. Array Partition
+ */
+public class ArrayPartition {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+
+        int sum = 0;
+        for(int i=0; i<nums.length; i+=2) {
+            sum += nums[i];
+        }
+
+        return sum;
+    }
+}
