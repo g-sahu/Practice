1de0049cd26841450756471fceba07fc1f23c8a9
Gaurav Sahu <simplygaurav07@gmail.com>
2026-08-04 14:29:10 +0530

    Leetcode #1700

---
+++ src/com/practice/leetcode/NumOfStudentsUnableToEatLunch.java
@@ -0,0 +1,35 @@
+package com.practice.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 1700. Number of Students Unable to Eat Lunch
+ */
+public class NumOfStudentsUnableToEatLunch {
+
+    public int countStudents(int[] students, int[] sw) {
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int student : students) {
+            q.add(student);
+        }
+
+        int i = 0;
+        int j = 0;
+
+        while(!q.isEmpty() && i<sw.length && j <= q.size()) {
+            int s = q.remove();
+
+            if(sw[i] == s) {
+                i++;
+                j = 0;
+            } else {
+                q.add(s);
+                j++;
+            }
+        }
+
+        return q.size();
+    }
+}
