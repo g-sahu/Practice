b21a8bdea4f5e3a963c226d5ccb4f5f6512fd771
Gaurav Sahu <simplygaurav07@gmail.com>
2026-08-08 20:14:55 +0530

    Leetcode #57

--- src/com/practice/leetcode/InsertInterval.java
+++ src/com/practice/leetcode/InsertInterval.java
@@ -8,45 +8,43 @@ import java.util.List;
  */
 public class InsertInterval {
 
-    public int[][] insert(int[][] arr, int[] n) {
-        if (arr.length == 0) {
-            return new int[][]{n};
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        int l = intervals.length;
+        int[][] arr = new int[l+1][];
+        int idx = l;
+
+        if(l == 0) {
+            return new int[][]{newInterval};
         }
 
-        int[][] intervals = new int[arr.length + 1][2];
-        boolean inserted = false;
-        int i = 0;
+        for(int i=0; i<l; i++) {
+            int[] curr = intervals[i];
 
-        for (int[] row : arr) {
-            if (!inserted && n[0] < row[0]) {
-                intervals[i] = n;
-                intervals[++i] = row;
-                inserted = true;
+            if(curr[0] < newInterval[0]) {
+                arr[i] = curr;
             } else {
-                intervals[i] = row;
+                idx = Math.min(idx, i);
+                arr[i+1] = curr;
             }
-            i++;
         }
 
-        if (!inserted) {
-            intervals[i] = n;
-        }
-
-        int start = intervals[0][0];
-        int end = intervals[0][1];
+        arr[idx] = newInterval;
+        int[] prev = arr[0];
         List<int[]> list = new ArrayList<>();
 
-        for (int[] row : intervals) {
-            if (row[0] > end) {
-                list.add(new int[]{start, end});
-                start = row[0];
-                end = row[1];
+        for(int i=1; i<arr.length; i++) {
+            int[] curr = arr[i];
+
+            if(curr[0] <= prev[1]) {
+                int end = Math.max(prev[1], curr[1]);
+                prev[1] = end;
             } else {
-                end = Math.max(end, row[1]);
+                list.add(prev);
+                prev = curr;
             }
         }
 
-        list.add(new int[]{start, end});
+        list.add(prev);
         return list.toArray(new int[list.size()][]);
     }
 
